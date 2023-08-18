2fda34540adfb8e83ea4db42e328211c88104631
Solarin <solarinjohnson@gmail.com>
2023-08-19 00:53:01 +0100

    changes

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-const apiKey = 'YOUR_API_KEY';
-const cityName = 'New York'; // Replace with the desired city name
 
-const apiUrl = `https://openweathermap.org/weather?city=${lagos}&apiKey=${ed51b93f114eb89ae8f996f98689cefe}`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Milan&appid=0fcd6a52b8947973e9492af60c94a84b`;
 
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        // Process and display weather data here
-        console.log(data);
+        const x = data.weather[0].description;
+        const temperature = data.main.temp;
+
+        console.log(`Weather: ${x}`);
+        console.log(`Temperature: ${temperature}°C`);
     })
     .catch(error => {
-        console.error('Error fetching weather data:', error);
+        console.error('Error fetching data:', error);
     });
+autoResize = setInterval(() => {
+    if (innerHeight < 500) {
+        document.body.style.height = 500 + "px"
+    } else {
+        document.body.style.height = innerHeight + "px"
+    }
+}, 1);
