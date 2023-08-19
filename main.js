2025ac624e672399f46c1cfe9fd752aeee92a9b2
Solarin <solarinjohnson@gmail.com>
2023-08-19 03:14:01 +0100

    final commit

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
+document.querySelectorAll("#btn")[0].addEventListener('click', () => {
+    city = document.getElementById("city_name").value
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q= ${city} &appid=0fcd6a52b8947973e9492af60c94a84b`;
 
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Milan&appid=0fcd6a52b8947973e9492af60c94a84b`;
 
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        const x = data.weather[0].description;
-        const temperature = data.main.temp;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const description = data.weather[0].description;
+            const temperature = data.main.temp;
 
-        console.log(`Weather: ${x}`);
-        console.log(`Temperature: ${temperature}°C`);
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
+            console.log(`Weather: ${description}`);
+            console.log(`Temperature: ${temperature}°C`);
+            deg = `&deg`
+            result = `${city} has ${description} currently, with a temperature of about ${Math.round(temperature)} °C equivalent to ${(Math.round(temperature * (9 / 5)) + 32)} F`
+            console.log(result)
+            document.getElementById("results").textContent = result
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+
+    
+})
 autoResize = setInterval(() => {
     if (innerHeight < 500) {
         document.body.style.height = 500 + "px"
@@ -20,3 +29,4 @@ autoResize = setInterval(() => {
         document.body.style.height = innerHeight + "px"
     }
 }, 1);
+
